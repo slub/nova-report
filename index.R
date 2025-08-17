54106c1ebeb1538792d89582b62d6962208eec6a
herreio <47579252+herreio@users.noreply.github.com>
2025-08-17 19:10:54 +0000

    Deploying to gh-pages from @ slub/nova-report@e38b12a4fa3b14cd302246aad7d9826c050edbfa 🚀

--- index.R
+++ index.R
@@ -7,10 +7,22 @@ knitr::opts_knit$set(root.dir = "..")
 newitem_records_series <- readRDS("data/newitem-records-series.RDS")
 
 
-## ----newitem-records-plot, eval=F---------------------------------------------
-# ggplot2::ggplot(newitem_records_series, ggplot2::aes(x = day, y = count)) +
-#   ggplot2::geom_col() +
-#   ggplot2::xlab("") +
-#   ggplot2::ylab("Anzahl Neuerwerbungen") +
-#   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+## ----newitem-records-plot-----------------------------------------------------
+ggplot2::ggplot(newitem_records_series, ggplot2::aes(x = day, y = count)) +
+  ggplot2::geom_col() +
+  ggplot2::xlab("Aktivierungsdatum") +
+  ggplot2::ylab("Anzahl Titel") +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+
+
+## ----purchase-records-input---------------------------------------------------
+purchase_records_series <- readRDS("data/purchase-records-series.RDS")
+
+
+## ----purchase-records-plot----------------------------------------------------
+ggplot2::ggplot(purchase_records_series, ggplot2::aes(x = day, y = count)) +
+  ggplot2::geom_col() +
+  ggplot2::xlab("Erwerbungsdatum") +
+  ggplot2::ylab("Anzahl Exemplare") +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
 
