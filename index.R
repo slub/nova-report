0074b960544a2b063a2fb302aade61b50e41a674
herreio <47579252+herreio@users.noreply.github.com>
2025-08-17 20:00:20 +0000

    Deploying to gh-pages from @ slub/nova-report@90aceeb87bfc341b08020f26c98746f589540862 🚀

--- index.R
+++ index.R
@@ -3,6 +3,18 @@ knitr::opts_chunk$set(echo = FALSE, error = FALSE, message = FALSE, warning = FA
 knitr::opts_knit$set(root.dir = "..")
 
 
+## ----indexed-records-input----------------------------------------------------
+indexed_records_series <- readRDS("data/indexed-records-series.RDS")
+
+
+## ----indexed-records-plot, eval=F---------------------------------------------
+# ggplot2::ggplot(indexed_records_series, ggplot2::aes(x = day, y = count)) +
+#   ggplot2::geom_col() +
+#   ggplot2::xlab("Erstindexierungsdatum") +
+#   ggplot2::ylab("Anzahl Titel") +
+#   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+
+
 ## ----newitem-records-input----------------------------------------------------
 newitem_records_series <- readRDS("data/newitem-records-series.RDS")
 
