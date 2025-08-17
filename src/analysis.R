bcd9f307966b1f2953279f6bb6b9d4e1333cbe06
Donatus Herre <donatus.herre@slub-dresden.de>
2025-08-17 19:32:34 +0200

    Use serialized data in report

---
+++ src/analysis.R
@@ -0,0 +1,36 @@
+# VuFind Solr : first_indexed
+
+indexed24_records <- read_zenodo_jsonl_gz_local("https://zenodo.org/records/16881662/files/slub-nova-2024-first-indexed.jsonl.gz?download=1")
+indexed23_records <- read_zenodo_jsonl_gz_local("https://zenodo.org/records/16881739/files/slub-nova-2023-first-indexed.jsonl.gz?download=1")
+indexed22_records <- read_zenodo_jsonl_gz_local("https://zenodo.org/records/16858259/files/slub-nova-2022-first-indexed.jsonl.gz?download=1")
+
+indexed_records <- rbind(indexed22_records, indexed23_records, indexed24_records)
+indexed_records <- indexed_records[with(indexed_records, order(first_indexed, id)), ]
+
+# Libero LMS : NewItemActDate
+
+newitem24_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/16385159/files/slub-nova-2024-new-item-act-date.jsonl.gz?download=1")
+newitem23_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/10888494/files/slub-nova-2023-new-item-act-date.jsonl.gz?download=1")
+newitem22_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/10179459/files/slub-nova-2022-new-item-act-date.jsonl.gz?download=1")
+
+newitem_records <- rbind(newitem23_records, newitem24_records)
+newitem_records_legacy <- rbind(newitem22_records, newitem23_records[, -2], newitem24_records[, -2])
+
+newitem_records_series <- eval_date_values(newitem_records_legacy$de14_new_item_act_date_mv)
+saveRDS(newitem_records_series, "data/newitem-records-series.RDS")
+
+# Libero LMS : DatePurchased
+
+purchase24_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/16385169/files/slub-nova-2024-purchase-date.jsonl.gz?download=1")
+purchase23_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/10888524/files/slub-nova-2023-purchase-date.jsonl.gz?download=1")
+purchase22_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/10251277/files/slub-nova-2022-purchase-date.jsonl.gz?download=1")
+
+purchase24_acquisition_type <- read_zenodo_csv("https://zenodo.org/records/16385169/files/slub-nova-2024-acquisition-code.csv?download=1")
+purchase23_acquisition_type <- read_zenodo_csv("https://zenodo.org/records/10888524/files/slub-nova-2023-acquisition-code.csv?download=1")
+purchase22_acquisition_type <- read_zenodo_csv("https://zenodo.org/records/10251277/files/slub-nova-2022-acquisition-code.csv?download=1")
+
+purchase24_subject_category <- read_zenodo_csv("https://zenodo.org/records/16385169/files/slub-nova-2024-statistic-code.csv?download=1")
+purchase23_subject_category <- read_zenodo_csv("https://zenodo.org/records/10888524/files/slub-nova-2023-statistic-code.csv?download=1")
+
+purchase_records <- rbind(purchase23_records, purchase24_records)
+purchase_records_legacy <- rbind(purchase22_records, purchase23_records[, -c(2, 3, 4, 8)], purchase24_records[, -c(2, 3, 4, 8)])
---
+++ src/utils.R
@@ -0,0 +1,47 @@
+read_zenodo_jsonl_gz <- function(in_path) {
+  jsonlite::stream_in(
+    gzcon(url(in_path)),
+    verbose = FALSE
+  )
+}
+
+read_zenodo_jsonl_gz_local <- function(in_path) {
+  jsonl_gz_file <- tempfile(fileext = ".jsonl.gz")
+  curl::curl_download(
+    in_path,
+    jsonl_gz_file
+  )
+  record_data <- jsonlite::stream_in(
+    file(jsonl_gz_file),
+    verbose = FALSE
+  )
+  unlink(jsonl_gz_file)
+  record_data
+}
+
+read_zenodo_csv <- function(in_path) {
+  readr::read_csv(
+    in_path,
+    show_col_types = FALSE
+  )
+}
+
+read_json_simple <- function(in_path) {
+  jsonlite::read_json(
+    in_path
+  )
+}
+
+eval_values <- function(field) {
+  field_stats <- table(unlist(field))
+  data.frame(value = names(field_stats), count = as.integer(field_stats))
+}
+
+eval_date_values <- function(field) {
+  field_df <- eval_values(field)
+  field_df$value <- gsub("T00:00:00Z", "", field_df$value)
+  field_df[order(field_df$value), ]
+  names(field_df) <- c("day", "count")
+  field_df$day <- as.Date(field_df$day)
+  field_df
+}
