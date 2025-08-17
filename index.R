7b6d2e8e29c6c28032afbeb6cb9a0e9b98a22228
herreio <47579252+herreio@users.noreply.github.com>
2025-08-17 18:01:34 +0000

    Deploying to gh-pages from @ slub/nova-report@141c6977f059e9ff7b6dd05cd34077f781943af6 🚀

--- index.R
+++ index.R
@@ -7,10 +7,10 @@ knitr::opts_knit$set(root.dir = "..")
 newitem_records_series <- readRDS("data/newitem-records-series.RDS")
 
 
-## ----newitem-records-plot-----------------------------------------------------
-ggplot2::ggplot(newitem_records_series, ggplot2::aes(x = day, y = count)) +
-  ggplot2::geom_col() +
-  ggplot2::xlab("") +
-  ggplot2::ylab("Anzahl Neuerwerbungen") +
-  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
+## ----newitem-records-plot, eval=F---------------------------------------------
+# ggplot2::ggplot(newitem_records_series, ggplot2::aes(x = day, y = count)) +
+#   ggplot2::geom_col() +
+#   ggplot2::xlab("") +
+#   ggplot2::ylab("Anzahl Neuerwerbungen") +
+#   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
 
