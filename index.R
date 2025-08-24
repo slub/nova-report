e181890cf649ee76ca74dfb8ed21e3e723a54222
herreio <47579252+herreio@users.noreply.github.com>
2025-08-24 11:16:15 +0000

    Deploying to gh-pages from @ slub/nova-report@4f7f8d86384412eb3bb6f4330d2fdf1c137b5efe 🚀

--- index.R
+++ index.R
@@ -10,7 +10,7 @@ indexed_records_series <- readRDS("data/indexed-records-series.RDS")
 ## ----indexed-records-plot-----------------------------------------------------
 ggplot2::ggplot(indexed_records_series, ggplot2::aes(x = day, y = count)) +
   ggplot2::geom_col() +
-  ggplot2::xlab("Erstindexierungsdatum") +
+  ggplot2::xlab("Indexierungsdatum") +
   ggplot2::ylab("Anzahl Titel") +
   ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60, hjust = 1))
 
