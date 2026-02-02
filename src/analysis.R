30268e6ad221709f55280e154e8d1556b72a100d
Donatus Herre <donatus.herre@slub-dresden.de>
2026-02-02 21:06:04 +0100

    Rename series to series daily

--- src/analysis.R
+++ src/analysis.R
@@ -7,9 +7,9 @@ indexed22_records <- read_zenodo_jsonl_gz_local("https://zenodo.org/records/1685
 indexed_records <- rbind(indexed22_records, indexed23_records, indexed24_records)
 indexed_records <- indexed_records[with(indexed_records, order(first_indexed, id)), ]
 
-indexed_records_series <- eval_datetime_values(indexed_records$first_indexed)
-readr::write_csv(indexed_records_series, "data-raw/indexed-records-series.csv")
-saveRDS(indexed_records_series, "data/indexed-records-series.RDS")
+indexed_records_series_daily <- eval_datetime_values(indexed_records$first_indexed)
+readr::write_csv(indexed_records_series_daily, "data-raw/indexed-records-series-daily.csv")
+saveRDS(indexed_records_series_daily, "data/indexed-records-series-daily.RDS")
 
 indexed_records_series_monthly <- eval_month_values(indexed_records$first_indexed)
 readr::write_csv(indexed_records_series_monthly, "data-raw/indexed-records-series-monthly.csv")
@@ -28,9 +28,9 @@ newitem22_records <- read_zenodo_jsonl_gz("https://zenodo.org/records/10179459/f
 newitem_records <- rbind(newitem23_records, newitem24_records)
 newitem_records_legacy <- rbind(newitem22_records, newitem23_records[, -2], newitem24_records[, -2])  # drop field rsn_id_str_mv
 
-newitem_records_series <- eval_date_values(newitem_records_legacy$de14_new_item_act_date_mv)
-readr::write_csv(newitem_records_series, "data-raw/newitem-records-series.csv")
-saveRDS(newitem_records_series, "data/newitem-records-series.RDS")
+newitem_records_series_daily <- eval_date_values(newitem_records_legacy$de14_new_item_act_date_mv)
+readr::write_csv(newitem_records_series_daily, "data-raw/newitem-records-series-daily.csv")
+saveRDS(newitem_records_series_daily, "data/newitem-records-series-daily.RDS")
 
 newitem_records_series_monthly <- eval_month_values(unlist(newitem_records_legacy$de14_new_item_act_date_mv))
 readr::write_csv(newitem_records_series_monthly, "data-raw/newitem-records-series-monthly.csv")
@@ -66,9 +66,9 @@ purchase_subject_category <- purchase_subject_category[with(purchase_subject_cat
 #readr::write_csv(purchase_subject_category, "data-raw/purchase-subject-category.csv")
 #saveRDS(purchase_subject_category, "data/purchase-subject-category.RDS")
 
-purchase_records_series <- eval_date_values(purchase_records_legacy$de14_purchase_date_mv)
-readr::write_csv(purchase_records_series, "data-raw/purchase-records-series.csv")
-saveRDS(purchase_records_series, "data/purchase-records-series.RDS")
+purchase_records_series_daily <- eval_date_values(purchase_records_legacy$de14_purchase_date_mv)
+readr::write_csv(purchase_records_series_daily, "data-raw/purchase-records-series-daily.csv")
+saveRDS(purchase_records_series_daily, "data/purchase-records-series-daily.RDS")
 
 purchase_records_series_monthly <- eval_month_values(unlist(purchase_records_legacy$de14_purchase_date_mv))
 readr::write_csv(purchase_records_series_monthly, "data-raw/purchase-records-series-monthly.csv")
